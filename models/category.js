795c0c25192be7eae7e92a9e102c60f934ef2c70
whatevermane <zmatt33@gmail.com>
2019-05-09 19:19:03 -0500

    renamed index.html

--- models/category.js
+++ models/category.js
@@ -1,7 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   var Category = sequelize.define("Category", {
     name: DataTypes.STRING,
-    img: DataTypes.STRING,
     description: DataTypes.TEXT
   });
   return Category;
