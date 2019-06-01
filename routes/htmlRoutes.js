f7bb291a2def758e3cd040c6c077bfb4ca76273d
thuyngavo <lisa.vo49@gmail.com>
2019-06-01 13:58:35 -0500

    fixing cart.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -47,16 +47,6 @@ module.exports = function(app) {
   });
 
   //cart page
-  // app.get("/cart", function(req, res) {
-  //   db.Cart.findAll({}).then(function(dbCart) {
-  //     res.render("cart", {
-  //       msg: "Welcome to the Shopping cart!",
-  //       examples: dbCart
-  //     });
-  //   });
-  // });
-
-  //cart page
   app.get("/order", function(req, res) {
     db.Cart.findAll({}).then(function(dbCart) {
       res.render("checkout", {
